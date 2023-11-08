1787f1638b5e2d0655a1e448f1323915a9213657
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-11-08 09:09:49 -0300

    finalizando o formulario

--- frontend/moments/src/app/app.module.ts
+++ frontend/moments/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
     AboutComponent,
     HomeComponent,
     FooterComponent,
-    NewMomentComponent
+    NewMomentComponent,
+    MomentFormComponent
   ],
   imports: [
     BrowserModule,
